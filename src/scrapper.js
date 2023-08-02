0607bd3fdc16e541b83c510653bfaade7edc77e4
Lenstack <42081538+Lenstack@users.noreply.github.com>
2023-08-02 09:49:40 -0500

    Add Adblocker And Without Captcha Quiz

--- src/scrapper.js
+++ src/scrapper.js
@@ -1,8 +1,25 @@
 const puppeteer = require("puppeteer");
+
 const scrapper = async (steps, options, items) => {
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080});
+    await page.setRequestInterception(true);
+
+    const rejectRequestPattern = [
+        "googlesyndication.com",
+        "/*.doubleclick.net",
+        "/*.amazon-adsystem.com",
+        "/*.adnxs.com",
+    ];
+    const blockList = [];
+
+    page.on("request", (request) => {
+        if (rejectRequestPattern.find((pattern) => request.url().match(pattern))) {
+            blockList.push(request.url());
+            request.abort();
+        } else request.continue();
+    });
 
     try {
         console.log("Starting");
