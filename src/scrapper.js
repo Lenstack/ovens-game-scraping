359b67ffd9916fc245baa0d6b277d57af1e9e1c5
Lenstack <42081538+Lenstack@users.noreply.github.com>
2023-08-12 14:30:48 -0500

    Update

--- src/scrapper.js
+++ src/scrapper.js
@@ -1,7 +1,9 @@
 const puppeteer = require("puppeteer");
+const PCR = require("puppeteer-chromium-resolver");
 
 const scrapper = async (steps, options, items) => {
-    const browser = await puppeteer.launch(options);
+    const stats = await PCR({});
+    const browser = await puppeteer.launch({...options, executablePath: stats.executablePath});
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080});
     await page.setRequestInterception(true);
